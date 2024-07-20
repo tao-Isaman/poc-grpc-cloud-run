ccd6649ec6f52b34fbfcbe492f5226df794fdbb6
tao-isaman <towlovelove@gmail.com>
2024-07-20 16:16:07 +0700

    update port to 8080

--- server/main.go
+++ server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -19,14 +21,23 @@ func (s *server) SayHello(ctx context.Context, req *hello.HelloRequest) (*hello.
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	grpcEndpoint := fmt.Sprintf(":%s", port)
+	log.Printf("gRPC endpoint [%s]", grpcEndpoint)
+
+	lis, err := net.Listen("tcp", grpcEndpoint)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
 	hello.RegisterHelloServiceServer(s, &server{})
 	reflection.Register(s)
-	log.Println("gRPC server listening on port 50051")
+	log.Printf("gRPC server listening on port %s", port)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
